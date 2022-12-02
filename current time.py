55999498eff572f9d5d31bad524ec9b1966780c9
programmer0101 <113084773+Yousuf9963@users.noreply.github.com>
2022-12-02 19:57:07 +0300

    Uploaded whoami.py

---
+++ current time.py
@@ -0,0 +1,17 @@
+print("[&] Coded By Yousuf Shafi'i Muhammad Junior Programmer")
+print("")
+print("[-] Website muhammadabdirahman.wixsite.com/yousuf9963blog")
+print("")
+print("This Program will tell you your current time")
+print("")
+
+print(" I hope for you good future and i am willing that you will come high effort.")
+print("")
+
+import time
+
+t = time.localtime(time.time())
+localtime = time.asctime(t)
+str ="Your Current Time is:" + time.asctime(t)
+
+print(str);
